1bde7eb9b2fc615b6874039fdb363730807169ba
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-04-10 20:45:27 +0300

    Fixes

--- CustomModules/VW6872274481.lua
+++ CustomModules/VW6872274481.lua
@@ -3251,7 +3251,7 @@ run(function()
 	})
 end)
 
-run(function()
+--[[run(function()
 	local AntiHit = {}
 	local physEngine = game:GetService("RunService")
 	local worldSpace = game.Workspace
@@ -3427,7 +3427,7 @@ run(function()
 		Suffix = function(v) return v == 1 and "span" or "spans" end,
 		Function = function(v) scanRad = v end
 	})
-end)
+end)--]]
 
 --[[run(function()
 	local MelodyExploit = {Enabled = false}
