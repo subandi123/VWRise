ac5bfc9b7dc6737a508f98a85cf0b5771b2a1ca5
fdpclienteee <74751653+subandi123@users.noreply.github.com>
2025-06-20 19:15:50 +0800

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -3,7 +3,6 @@ local player = Players.LocalPlayer
 local PlayerGui = player:WaitForChild("PlayerGui")
 local tictactoeGui = PlayerGui:WaitForChild("TicTacToe")
 
-
 local modules = {}
 local remotes = {}
 local board = {}
@@ -32,38 +31,7 @@ for _, gc in pairs(getgc(true)) do
                 local choicePanel = tictactoeGui:FindFirstChild("Bottom Middle")
                 
                 if choicePanel and choicePanel:FindFirstChild("Buttons") then
-                    for _, button in ipairs(choicePanel.Buttons:GetChildren()) do
-                        if button:IsA("TextButton") then
-                            local slotNumberStr = string.split(button.Name, "Drop_")[2]
-                            local slotIndex = tonumber(slotNumberStr)
-                            
-                            if slotIndex then
-                                local boardSlotName = SLOT_MAPPING[slotIndex]
-                                local boardSlot = boardModel and boardModel:FindFirstChild(boardSlotName)
-                                
-                                if boardSlot then
-                                    local full = boardSlot:GetAttribute("Full")
-                                    local row = tonumber(string.sub(boardSlotName, 1, 1))
-                                    local col = tonumber(string.sub(boardSlotName, 2, 2))
-                                    
-                                    if full == "Red" then
-                                        board[row][col] = 1
-                                    elseif full == "blue" then
-                                        board[row][col] = 2
-                                    else
-                                        board[row][col] = 0
-                                    end
-                                end
-                            end
-                        end
-                    end
-                    for i,v in pairs(board) do
-                        print("{")
-                        for i,v2 in pairs(v) do
-                            print(i,v2, "Hi")
-                        end
-                        print("}")
-                    end
+                   print("yes")
                 end
                 
                 return originalFunction(...)
