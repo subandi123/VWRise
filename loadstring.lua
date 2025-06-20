ff0a17b4b0937913c4becd7b5d7d543fa2914ead
fdpclienteee <74751653+subandi123@users.noreply.github.com>
2025-06-20 19:27:53 +0800

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -3,6 +3,7 @@ local player = Players.LocalPlayer
 local PlayerGui = player:WaitForChild("PlayerGui")
 local tictactoeGui = PlayerGui:WaitForChild("TicTacToe")
 
+
 local modules = {}
 local remotes = {}
 local board = {}
@@ -25,14 +26,38 @@ for _, gc in pairs(getgc(true)) do
     if type(gc) == "function" then
         local info = debug.getinfo(gc)
         if info.name == "AskForChoice" and string.find(info.source:lower(), "tictactoe") then
-            local originalFunction; originalFunction = hookfunction(gc, function(...)
+            local originalFunction
+            originalFunction = hookfunction(gc, function(...)
                 local boardModel = select(2, ...)
-                print("BoardModel type:", typeof(boardModel))
-                local choicePanel = tictactoeGui:FindFirstChild("Bottom Middle")
-                print(boardModel, choicePanel)
                 
-                if choicePanel and choicePanel:FindFirstChild("Buttons") then
-                   print("yes")
+                if typeof(boardModel) == "Instance" then
+                    for slotIndex=1,9 do
+                        if slotIndex then
+                            local boardSlotName = SLOT_MAPPING[slotIndex]
+                            local boardSlot = boardModel and boardModel:FindFirstChild(boardSlotName)
+                            
+                            if boardSlot then
+                                local full = boardSlot:GetAttribute("Full")
+                                local row = tonumber(string.sub(boardSlotName, 1, 1))
+                                local col = tonumber(string.sub(boardSlotName, 2, 2))
+                                
+                                if full == "Red" then
+                                    board[row][col] = 1
+                                elseif full == "blue" then
+                                    board[row][col] = 2
+                                else
+                                    board[row][col] = 0
+                                end
+                            end
+                        end
+                    end
+                    for i,v in pairs(board) do
+                        print("{")
+                        for i,v2 in pairs(v) do
+                            print(i,v2, "Hi")
+                        end
+                        print("}")
+                    end
                 end
                 
                 return originalFunction(...)
